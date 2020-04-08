578c6e20594215055c8c590f9a54c4099b2bc035
Jonne <jonne@jonnesaleva.com>
2020-04-08 15:23:13 -0400

    add moses-detokenize.sh

---
+++ scripts/moses-detokenize.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+# Convenience wrapper for detokenization in Moses 
+# Jonne Saleva, 2020
+
+DETOKENIZER_SCRIPT=$MOSES_SCRIPTS/tokenizer/detokenizer.perl
+LANGUAGE=$1
+INPUT_FILE=$2
+OUTPUT_FILE=$3
+
+$DETOKENIZER_SCRIPT \
+    -l $LANGUAGE \
+    < $INPUT_FILE \
+    > $OUTPUT_FILE
+
